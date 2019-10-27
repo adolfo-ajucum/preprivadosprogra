253f0007ee60c0bec9f1bc26e2efa03509bc1f40
Martin <martin_aj22@hotmail.com>
2019-10-27 11:48:08 -0600

    3 ejercicios

--- main.cpp
+++ main.cpp
@@ -12,14 +12,67 @@
  */
 
 #include <cstdlib>
+#include <cstdlib>
+#include <stdlib.h>
+#include "iostream"
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+void corredores(){
+  int contador = 0;
+    float v1;
+    string nombre;
+    for(int i=0; i<=10; i++){
+        v1 = rand() % 60;
+        cout<<"Ingrese nombre: ";
+        cin>>nombre;
+        if(v1<=20){
+         cout<<"tiempo: "<<v1<<" Resultado: Recibido en el equipo de atletismo ";
+         contador++;
+        }else{
+            cout<<"tiempo: "<<v1<<" Gracias por participar"<<endl;
+        }
+    }
+    cout<<"-----------------------------"<<endl;
+    cout<<"Numero de corredores clasificados: "<<contador<<endl;  
+}
 
+void materias(){
+    string materia;
+    
+    float  nota, total=0;
+    
+    for(int i=0; i<5; i++){
+        cout<<"Ingrese nombre materia: "; cin>>materia;
+        cout<<"Ingrese nota:"; cin>>nota;
+        total=total+nota;
+    }
+    cout<<"El primedio es: "<<total/5;
+}
+
+void telas(){
+    int op;
+    float aux,total;
+    string nombre;
+    do{
+        cout<<"Ingrese nombre de la prenda: "; cin>>nombre;
+        cout<<"Ingrese yardas: "; cin>> aux;
+        total=total+aux;  
+        cout<<"Presione 1 para continuar y 0 para terminar ";
+        cin>>op;
+    }while(op==1);
+    cout<<"-----------------------------"<<endl;
+    cout<<"El total de yardas es: "<<total<<endl;
+}
+    
+int main(int argc, char** argv) {
+    //corredores();
+    //materias();
+    telas();
+    
     return 0;
 }
 
