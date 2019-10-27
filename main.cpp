378ebe1cdb7f4182dd620a3070791eaa40c440e4
Martin <martin_aj22@hotmail.com>
2019-10-27 12:04:37 -0600

    se agrego menu

--- main.cpp
+++ main.cpp
@@ -21,68 +21,114 @@ using namespace std;
 /*
  * 
  */
-void corredores(){
-  int contador = 0;
+void corredores() {
+    int contador = 0;
     float v1;
     string nombre;
-    for(int i=0; i<=10; i++){
+    for (int i = 0; i <= 10; i++) {
         v1 = rand() % 60;
-        cout<<"Ingrese nombre: ";
+        cout << "Ingrese nombre: ";
         cin>>nombre;
-        if(v1<=20){
-         cout<<"tiempo: "<<v1<<" Resultado: Recibido en el equipo de atletismo ";
-         contador++;
-        }else{
-            cout<<"tiempo: "<<v1<<" Gracias por participar"<<endl;
+        if (v1 <= 20) {
+            cout << "tiempo: " << v1 << " Resultado: Recibido en el equipo de atletismo ";
+            contador++;
+        } else {
+            cout << "tiempo: " << v1 << " Gracias por participar" << endl;
         }
     }
-    cout<<"-----------------------------"<<endl;
-    cout<<"Numero de corredores clasificados: "<<contador<<endl;  
+    cout << "-----------------------------" << endl;
+    cout << "Numero de corredores clasificados: " << contador << endl;
 }
 
-void materias(){
+void materias() {
     string materia;
-    
-    float  nota, total=0;
-    
-    for(int i=0; i<5; i++){
-        cout<<"Ingrese nombre materia: "; cin>>materia;
-        cout<<"Ingrese nota:"; cin>>nota;
-        total=total+nota;
+
+    float nota, total = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Ingrese nombre materia: ";
+        cin>>materia;
+        cout << "Ingrese nota:";
+        cin>>nota;
+        total = total + nota;
     }
-    cout<<"El primedio es: "<<total/5;
+    cout << "El primedio es: " << total / 5;
 }
 
-void telas(){
+void telas() {
     int op;
-    float aux,total;
+    float aux, total;
     string nombre;
-    do{
-        cout<<"Ingrese nombre de la prenda: "; cin>>nombre;
-        cout<<"Ingrese yardas: "; cin>> aux;
-        total=total+aux;  
-        cout<<"Presione 1 para continuar y 0 para terminar ";
+    do {
+        cout << "Ingrese nombre de la prenda: ";
+        cin>>nombre;
+        cout << "Ingrese yardas: ";
+        cin>> aux;
+        total = total + aux;
+        cout << "Presione 1 para continuar y 0 para terminar ";
         cin>>op;
-    }while(op==1);
-    cout<<"-----------------------------"<<endl;
-    cout<<"El total de yardas es: "<<total<<endl;
+    } while (op == 1);
+    cout << "-----------------------------" << endl;
+    cout << "El total de yardas es: " << total << endl;
 }
-void facotrial(){
-   int n,i;
-   long double factorial; // se declara long double para poder representar números grandes
-   cout << "Introduce un numero: ";
-   cin >> n;
-   factorial=1;
-   for(i=1;i<=n;i++)
-   factorial = factorial * i;
-   cout << endl << "Factorial de " << n << " -> " << factorial << endl;
-}   
+
+void facotrial() {
+    int n, i;
+    long double factorial; // se declara long double para poder representar números grandes
+    cout << "Introduce un numero: ";
+    cin >> n;
+    factorial = 1;
+    for (i = 1; i <= n; i++)
+        factorial = factorial * i;
+    cout << endl << "Factorial de " << n << " -> " << factorial << endl;
+}
+
 int main(int argc, char** argv) {
     //corredores();
     //materias();
     //telas();
-    facotrial();
-    
+    //facotrial();
+    int choice;
+
+    do {
+
+
+        cout << endl
+                << " 1 - Corredores.\n"
+                << " 2 - Notas.\n"
+                << " 3 - Telas.\n"
+                << " 4 - Factorial.\n"
+                << " 5 - Salir.\n"
+                << " Elije una opcion y presiona enter: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                //Ejercicio1
+                corredores();
+                break;
+            case 2:
+                //Ejercicio2
+                materias();
+                break;
+            case 3:
+                //Ejercicio3
+                telas();
+                break;
+            case 4:
+                //Ejercicio 4
+                facotrial();
+                break;
+            case 5:
+                cout << "Fin del programa.\n";
+                break;
+            default:
+                cout << "No es una opcion valida. \n"
+                        << "Elija de nuevo.\n";
+                break;
+        }
+
+    } while (choice != 5);
     return 0;
 }
 
